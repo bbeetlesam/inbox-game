dfd89cfbc01b7e8f5352c8ba18e97b0bb2432894
bbeetlesam <samsismines@gmail.com>
2025-04-18 03:53:14 +0700

    set the letterbox with utils (took it from itera lol)

--- main.lua
+++ main.lua
@@ -1,13 +1,30 @@
+-- Main entry of the game (LÖVE default)
+
+local utils = require("src/utils")
+
+-- Load the game
+-- This function is called once at the beginning of the game
 function love.load()
     
 end
 
+-- Update the game
+-- This function is called every frame
 function love.update(dt)
-    
+    Game = {utils.setGameScreen(1440, 1080)}
 end
 
+-- Draw the game
+-- This function is called every frame
 function love.draw()
-    
+    love.graphics.push()
+    love.graphics.translate(Game[1], Game[2])
+    love.graphics.scale(Game[3], Game[4])
+
+    utils.setBaseBgColor(0.2, 0.2, 0.3)
+    love.graphics.rectangle("fill", 0,0, 60,120)
+
+    love.graphics.pop()
 end
 
 function love.keypressed(key, scancode, isrepeat)
