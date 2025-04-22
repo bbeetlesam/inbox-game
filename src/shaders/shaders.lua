83ed87715ca2658204e4f8c00d4a8f98d6f14288
bbeetlesam <samsismines@gmail.com>
2025-04-22 12:48:02 +0700

    add: initShaders
    func to initiate shaders' values

--- src/shaders/shaders.lua
+++ src/shaders/shaders.lua
@@ -3,6 +3,11 @@ local shaders = {
     grainyNoise = love.graphics.newShader("src/shaders/grainy.glsl")
 }
 
+shaders.initShaders = function(barrelStrength, grainyStrength)
+    shaders.barrelDistortion:send("strength", barrelStrength or 0.06)
+    shaders.grainyNoise:send("strength", grainyStrength or 0.5)
+end
+
 shaders.updateShaders = function()
     shaders.grainyNoise:send("time", love.timer.getTime())
 end
