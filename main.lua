365a6ce4938474b4782a48a9505c74bc6f538171
bbeetlesam <samsismines@gmail.com>
2025-04-30 23:53:18 +0700

    SAVE PROGRESS
    save all progress in main.lua before i separate to other branches

--- main.lua
+++ main.lua
@@ -1,42 +1,72 @@
 -- Main entry of the game (LÖVE default)
 local utils = require("src/utils")
 local const = require("src/const")
+local state = require("src/state")
 local shaders = require("src/shaders/shaders")
+local cursor = require("src/cursor")
+local taskbar = require("src/taskbar")
+local apps = require("src/apps")
+local windows = require("src/windows")
 
-local canvas = love.graphics.newCanvas(const.game.screen.WIDTH, const.game.screen.HEIGHT)
+local mainCanvas = love.graphics.newCanvas(const.game.screen.WIDTH, const.game.screen.HEIGHT)
+local mainCanvas2 = love.graphics.newCanvas(const.game.screen.WIDTH, const.game.screen.HEIGHT)
 
 -- Load the game. Called once at the beginning of the game
 function love.load()
     love.window.setFullscreen(true)
+    love.mouse.setVisible(false)
     Game = {utils.core.setGameScreen(const.game.screen.WIDTH, const.game.screen.HEIGHT)}
 
-    shaders.barrelDistortion:send("strength", 0.05)
+    shaders.init()
 end
 
 -- Update the game. Called every frame
 function love.update(dt)
-
+    state.update()
+    shaders.update()
+    cursor.update()
+    apps.update(cursor)
 end
 
 -- Draw the game. Called every frame
 function love.draw()
-    love.graphics.setCanvas(canvas)
-    love.graphics.clear()
+    love.graphics.setCanvas(mainCanvas)
+        love.graphics.clear()
+        love.graphics.setShader(shaders.grainyNoise)
 
-    utils.core.setBaseBgColor(const.color.DEEP_TEAL)
-    love.graphics.setColor(const.color.SILVER_TASKBAR)
-    love.graphics.rectangle("fill", 0,const.game.screen.HEIGHT-60, const.game.screen.WIDTH,60)
-    love.graphics.rectangle("fill", 0,0, 100,100)
+        utils.core.setBaseBgColor(const.color.DEEP_TEAL)
 
+        taskbar.drawTaskbar()
+        apps.draw()
+        windows.draw()
+        cursor.draw()
+
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.setShader()
+    love.graphics.setCanvas()
+
+    love.graphics.setCanvas(mainCanvas2)
+        love.graphics.clear()
+        love.graphics.setShader(shaders.barrelDistortion)
+
+        love.graphics.draw(mainCanvas)
+
+        love.graphics.setShader()
     love.graphics.setCanvas()
 
     love.graphics.push()
     love.graphics.translate(Game[1], Game[2])
     love.graphics.scale(Game[3], Game[4])
 
-    love.graphics.setShader(shaders.barrelDistortion)
-    love.graphics.draw(canvas, 0, 0)
-    love.graphics.setShader()
+    love.graphics.draw(mainCanvas2)
+
+    -- shaders.applyShader(function()
+    --     utils.core.setBaseBgColor(const.color.DEEP_TEAL)
+    --     taskbar.drawTaskbar()
+    --     apps.draw()
+    --     cursor.draw()
+    -- end,
+    -- {shaders.grainyNoise, shaders.barrelDistortion})
 
     love.graphics.pop()
 end
@@ -46,6 +76,18 @@ function love.keyreleased(key, scancode, isrepeat)
         love.event.quit()
     elseif key == "f11" then
         utils.core.toggleFullscreen()
+    end
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then
+        apps.mousepressed(cursor)
+        windows.clickCheck(cursor)
+
+        if presses == 2 and apps.selectedApp.id ~= nil then
+            taskbar.addItem({apps.selectedApp.id, apps.selectedApp.icon})
+            windows.addItem({apps.selectedApp.id, apps.selectedApp.icon})
+        end
     end
 end
 
