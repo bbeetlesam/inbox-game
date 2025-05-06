b86586b22cdee28b70d84b78880c3334ae46cc78
bbeetlesam <samsismines@gmail.com>
2025-05-06 16:38:23 +0700

    feat: dragging window! and also some refactors
    add dragging window mechanism, use rectButton for header buttons, add type annotations to prevent future warnings, and that's it.

--- src/windows.lua
+++ src/windows.lua
@@ -1,11 +1,28 @@
 local utils = require ("src/utils")
 local const = require ("src/const")
+local cursor = require ("src/cursor")
+local taskbar = require ("src/taskbar")
+
+-- need this bcs there'd be a warning bout the type of the table in window.update()
+---@class WindowItem
+---@field x number
+---@field y number
 
 local window = {
     items = {},
-    headerFont = love.graphics.newFont(const.font.WIN95, 20),
+    header = {
+        height = 40,
+        font = love.graphics.newFont(const.font.WIN95, 20),
+        button = {
+            size = 25,
+        }
+    },
+    drag = {
+        offset = {x = 0, y = 0},
+        target = nil ---@type WindowItem|nil
+    },
 
-    size = {x = 800, y = 700, outline = 2}, -- for debugging reason
+    size = {x = 800, y = 700, outline = 1}, -- for debugging reason
 }
 
 function window.addItem(insertedItem)
@@ -24,6 +41,12 @@ function window.addItem(insertedItem)
 
         x = love.math.random(200, 800),
         y = love.math.random(200, 800),
+
+        hover = {
+            closeButton = false,
+            minimButton = false,
+            header = false,
+        },
     }
 
     table.insert(window.items, newItem)
@@ -41,19 +64,45 @@ function window.checkItemId(itemId)
     end
 end
 
-function window.cursorClickCheck(cursor)
+function window.closeWindow(itemId)
+    for i, item in ipairs(window.items) do
+        if item.id == itemId then
+            table.remove(window.items, i)
+            break
+        end
+    end
+
+    for i, item in ipairs(taskbar.items) do
+        if item.id == itemId then
+            table.remove(taskbar.items, i)
+            break
+        end
+    end
+end
+
+function window.cursorClickCheck(mousecCursor)
     for i = #window.items, 1, -1 do -- from topmost to bottom
         local item = window.items[i]
         local winX, winY, winW, winH = item.x, item.y, window.size.x, window.size.y
+        local headerX, headerY, headerW, headerH = item.x + 3, item.y + 3, window.size.x - 3*2, window.header.height - 3*2
 
-        if cursor.x >= winX and cursor.x <= winX + winW and cursor.y >= winY and cursor.y <= winY + winH then
+        -- Check if the mouse is inside header area (click to drag)
+        if item.hover.header then
+            if mousecCursor.x >= headerX and mousecCursor.x <= headerX + headerW and mousecCursor.y >= headerY and mousecCursor.y <= headerY + headerH then
+                window.drag.target = item
+                window.drag.offset.x = mousecCursor.x - item.x
+                window.drag.offset.y = mousecCursor.y - item.y
+            end
+        end
+
+        -- Check if mouse is inside window area (click to focus)
+        if mousecCursor.x >= winX and mousecCursor.x <= winX + winW and mousecCursor.y >= winY and mousecCursor.y <= winY + winH then
             -- Reorder window to last (top-most)
             table.remove(window.items, i)
             table.insert(window.items, item)
-            return true
+            return
         end
     end
-    return false
 end
 
 function window.draw()
@@ -64,8 +113,9 @@ function window.draw()
         love.graphics.setColor(const.color.SILVER_TASKBAR)
         love.graphics.rectangle("fill", item.x, item.y, window.size.x, window.size.y)
         -- window's header
+        item.hover.header = utils.rectButton(cursor, item.x + 3, item.y + 3, window.size.x - 3*2, window.header.height - 3*2)
         love.graphics.setColor(item.id == window.items[#window.items].id and const.color.NAVY_BLUE or utils.setRGB(130, 130, 130))
-        love.graphics.rectangle("fill", item.x + 3, item.y + 3, window.size.x - 3*2, 40 - 3*2)
+        love.graphics.rectangle("fill", item.x + 3, item.y + 3, window.size.x - 3*2, window.header.height - 3*2)
         -- window's white outline
         love.graphics.setColor(1, 1, 1)
         love.graphics.rectangle("fill", item.x, item.y, window.size.x, window.size.outline)
@@ -76,10 +126,26 @@ function window.draw()
         love.graphics.rectangle("fill", item.x, item.y + window.size.y - window.size.outline, window.size.x, window.size.outline)
         -- window's header text
         love.graphics.setColor(1, 1, 1)
-        love.graphics.setFont(window.headerFont)
+        love.graphics.setFont(window.header.font)
         love.graphics.print(windowLabel, item.x + 10, item.y + 10)
+        -- header buttons (close and minimize)
+        item.hover.closeButton = utils.rectButton(cursor, item.x + window.size.x - window.header.button.size - 15/2, item.y + 15/2, window.header.button.size, window.header.button.size)
+        item.hover.minimButton = utils.rectButton(cursor, item.x + window.size.x - window.header.button.size - 15/2 - 32.5, item.y + 15/2, window.header.button.size, window.header.button.size)
+        love.graphics.setColor(const.color.SILVER_TASKBAR)
+        love.graphics.rectangle("fill", item.x + window.size.x - window.header.button.size - 15/2, item.y + 15/2, window.header.button.size, window.header.button.size)
+        love.graphics.rectangle("fill", item.x + window.size.x - window.header.button.size - 15/2 - 32.5, item.y + 15/2, window.header.button.size, window.header.button.size)
     end
     love.graphics.setColor(1, 1, 1) -- reset color
+end
+
+function window.update(mouseCursor)
+    if love.mouse.isDown(1) and window.drag.target then
+        local item = window.drag.target
+        item.x = mouseCursor.x - window.drag.offset.x
+        item.y = mouseCursor.y - window.drag.offset.y
+    else
+        window.drag.target = nil
+    end
 end
 
 return window
