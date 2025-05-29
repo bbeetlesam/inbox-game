4d8fa8a0e3746b7ca23e154cc1b9e30408dfaf59
bbeetlesam <samsismines@gmail.com>
2025-05-30 03:21:55 +0700

    feat: set mechanism for settings' upper buttons
    refactor: use ifSelectedSection helper func to reduce code repetition, etc

--- src/apps/settings.lua
+++ src/apps/settings.lua
@@ -3,7 +3,9 @@ local utils = require("src/utils")
 
 local settings = {
     content = {
+        width = 0, height = 0,
         previousSettings = nil, -- to store previous settings before applying changes
+        selectedSection = "wallpaper",
     },
     font = {
         header = love.graphics.newFont(const.font.WIN95, 18),
@@ -17,11 +19,22 @@ local settings = {
         cancel = {isHovered = false, isClicked = false},
         apply = {isHovered = false, isClicked = false}
     },
-    upperButton = {},
+    upperButton = {
+        wallpaper = {height = 35, hoverRect = false},
+        general = {height = 35, hoverRect = false},
+        permissions = {height = 35, hoverRect = false},
+        about = {height = 35, hoverRect = false},
+    },
 }
 
+-- Helpers (to break long fvkin codes)
+local unpack = unpack or table.unpack -- portable across Lua versions
+local function ifSelectedSection(section, trueVal, falseVal)
+    return settings.content.selectedSection == section and trueVal or falseVal
+end
+
 settings.load = function(contentSizes)
-    settings.content = {width = contentSizes[1], height = contentSizes[2]}
+    settings.content.width, settings.content.height = contentSizes[1], contentSizes[2]
     settings.innerBevel.startX = settings.content.width/2 - settings.innerBevel.w/2
     settings.innerBevel.startY = settings.content.height/2 - settings.innerBevel.h/2
     settings.lowerButton.startX = settings.innerBevel.w - (settings.lowerButton.w*3 + settings.lowerButton.gapX*2) + 10
@@ -31,18 +44,24 @@ end
 
 settings.draw = function()
     -- inner rectangle
-    utils.bevelRect(settings.innerBevel.startX, settings.innerBevel.startY, settings.innerBevel.w, settings.innerBevel.h, 3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE)
+    utils.bevelRect(settings.innerBevel.startX, settings.innerBevel.startY, settings.innerBevel.w, settings.innerBevel.h,
+        3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE)
     -- upper buttons
+    local upperBArgs = {3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE, {1, 1, 0, 1}}
     love.graphics.setFont(settings.font.header)
-    utils.bevelRect(settings.innerBevel.startX, settings.upperButton.startY, 100, 35, 3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE, {1, 1, 0, 1})
-    utils.bevelRect(settings.innerBevel.startX + 100, settings.upperButton.startY, 80, 35, 3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE, {1, 1, 0, 1})
-    utils.bevelRect(settings.innerBevel.startX + 100 + 80, settings.upperButton.startY, 110, 35, 3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE, {1, 1, 0, 1})
-    utils.bevelRect(settings.innerBevel.startX + 100 + 80 + 110, settings.upperButton.startY, 70, 35, 3, const.color.SILVER_TASKBAR, const.color.SILVER_BEVEL, const.color.WHITE, {1, 1, 0, 1})
+    utils.bevelRect(settings.innerBevel.startX, settings.upperButton.startY - ifSelectedSection("wallpaper", 6, 0),
+        100, settings.upperButton.wallpaper.height, unpack(upperBArgs))
+    utils.bevelRect(settings.innerBevel.startX + 100, settings.upperButton.startY - ifSelectedSection("general", 6, 0),
+        80, settings.upperButton.general.height, unpack(upperBArgs))
+    utils.bevelRect(settings.innerBevel.startX + 100 + 80, settings.upperButton.startY - ifSelectedSection("permissions", 6, 0),
+        110, settings.upperButton.permissions.height, unpack(upperBArgs))
+    utils.bevelRect(settings.innerBevel.startX + 100 + 80 + 110, settings.upperButton.startY - ifSelectedSection("about", 6, 0),
+        70, settings.upperButton.about.height, unpack(upperBArgs))
     love.graphics.setColor(const.color.BLACK)
-    love.graphics.print("Wallpaper", settings.innerBevel.startX + 10, settings.upperButton.startY + 10)
-    love.graphics.print("General", settings.innerBevel.startX + 10 + 100, settings.upperButton.startY + 10)
-    love.graphics.print("Permissions", settings.innerBevel.startX + 10 + 100 + 80, settings.upperButton.startY + 10)
-    love.graphics.print("About", settings.innerBevel.startX + 10 + 100 + 80 + 110, settings.upperButton.startY + 10)
+    love.graphics.print("Wallpaper", settings.innerBevel.startX + 10, settings.upperButton.startY + 10 - ifSelectedSection("wallpaper", 3, 0))
+    love.graphics.print("General", settings.innerBevel.startX + 10 + 100, settings.upperButton.startY + 10 - ifSelectedSection("general", 3, 0))
+    love.graphics.print("Permissions", settings.innerBevel.startX + 10 + 100 + 80, settings.upperButton.startY + 10 - ifSelectedSection("permissions", 3, 0))
+    love.graphics.print("About", settings.innerBevel.startX + 10 + 100 + 80 + 110, settings.upperButton.startY + 10 - ifSelectedSection("about", 3, 0))
     -- 3 lower buttons
     local addPosLower = settings.lowerButton.w + settings.lowerButton.gapX
     utils.bevelRect(settings.lowerButton.startX, settings.lowerButton.startY, settings.lowerButton.w, settings.lowerButton.h, 3, const.color.SILVER_TASKBAR,
@@ -65,15 +84,33 @@ settings.update = function(mouseCursor, offsets)
     settings.lowerButton.ok.isHovered = utils.rectButton(cursor, settings.lowerButton.startX, settings.lowerButton.startY, settings.lowerButton.w, settings.lowerButton.h)
     settings.lowerButton.cancel.isHovered = utils.rectButton(cursor, settings.lowerButton.startX + addPosLower, settings.lowerButton.startY, settings.lowerButton.w, settings.lowerButton.h)
     settings.lowerButton.apply.isHovered = utils.rectButton(cursor, settings.lowerButton.startX + addPosLower*2, settings.lowerButton.startY, settings.lowerButton.w, settings.lowerButton.h)
+
+    settings.upperButton.wallpaper.hoverRect = utils.rectButton(cursor, settings.innerBevel.startX, settings.upperButton.startY, 100, 35)
+    settings.upperButton.general.hoverRect = utils.rectButton(cursor, settings.innerBevel.startX + 100, settings.upperButton.startY, 80, 35)
+    settings.upperButton.permissions.hoverRect = utils.rectButton(cursor, settings.innerBevel.startX + 100 + 80, settings.upperButton.startY, 110, 35)
+    settings.upperButton.about.hoverRect = utils.rectButton(cursor, settings.innerBevel.startX + 100 + 80 + 110, settings.upperButton.startY, 70, 35)
+
+    settings.upperButton.wallpaper.height = ifSelectedSection("wallpaper", 41, 35)
+    settings.upperButton.general.height = ifSelectedSection("general", 41, 35)
+    settings.upperButton.permissions.height = ifSelectedSection("permissions", 41, 35)
+    settings.upperButton.about.height = ifSelectedSection("about", 41, 35)
 end
 
 settings.firstClickedCheck = function()
+    -- Lower buttons
     if settings.lowerButton.ok.isHovered then
         settings.lowerButton.ok.isClicked = true
     elseif settings.lowerButton.cancel.isHovered then
         settings.lowerButton.cancel.isClicked = true
     elseif settings.lowerButton.apply.isHovered then
         settings.lowerButton.apply.isClicked = true
+    end
+
+    -- Upper buttons
+    if settings.upperButton.wallpaper.hoverRect then settings.content.selectedSection = "wallpaper"
+    elseif settings.upperButton.general.hoverRect then settings.content.selectedSection = "general"
+    elseif settings.upperButton.permissions.hoverRect then settings.content.selectedSection = "permissions"
+    elseif settings.upperButton.about.hoverRect then settings.content.selectedSection = "about"
     end
 end
 
