b0c75f8e11ae95e9ca886adb5dc73eb0ff60dd34
bbeetlesam <samsismines@gmail.com>
2025-06-27 04:04:42 +0700

    refactor: change folder's table structure to array
    easier to maintain and trigger expand folders

--- src/apps/fileFolders.lua
+++ src/apps/fileFolders.lua
@@ -1,22 +1,40 @@
 -- Folder and files for File Explorer app
 
 return {
-    ["My Computer"] = {
-        ["Documents"] = {
-            ["about.txt"] = "This game is shit, and you should'nt play it seriously.\nAnyway, I felt like the biggest asshole when I killed your rock 'n roll.",
-            ["lucy.txt"] = "Picture yourself in a boat on a river\nWith tangerine trees and marmalade skies\nSomebody calls you, you answer quite slowly\nA girl with kaleidoscope eyes"
-        },
-
-        ["Pictures"] = {
-
-        },
-
-        ["Music"] = {
-
-        },
-
-        ["Videos"] = {
-
-        },
+    {
+        name = "My Computer",
+        type = "folder",
+        expanded = true,
+        children = {
+            {
+                name = "Documents",
+                type = "folder",
+                expanded = true,
+                children = {
+                    { name = "about.txt", type = "file",
+                        content = "This game is shit, and you should'nt play it seriously.\nAnyway, I felt like the biggest asshole when I killed your rock 'n roll." },
+                    { name = "lucy.txt", type = "file",
+                        content = "Picture yourself in a boat on a river\nWith tangerine trees and marmalade skies\nSomebody calls you, you answer quite slowly\nA girl with kaleidoscope eyes" },
+                }
+            },
+            {
+                name = "Pictures",
+                type = "folder",
+                expanded = false,
+                children = {}
+            },
+            {
+                name = "Music",
+                type = "folder",
+                expanded = false,
+                children = {}
+            },
+            {
+                name = "Videos",
+                type = "folder",
+                expanded = false,
+                children = {}
+            },
+        }
     }
 }
