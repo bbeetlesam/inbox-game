1f8ad0897d6e29aa10595f65e43fd86fb826da1b
bbeetlesam <samsismines@gmail.com>
2025-07-02 22:40:53 +0700

    add: selected and size attributes on fileFolders

--- src/apps/fileFolders.lua
+++ src/apps/fileFolders.lua
@@ -5,15 +5,17 @@ return {
         name = "My Computer",
         type = "folder",
         expanded = true,
+        selected = false,
         children = {
             {
                 name = "Documents",
                 type = "folder",
                 expanded = true,
+                selected = false,
                 children = {
-                    { name = "about.txt", type = "file-text",
-                        content = "This game is shit, and you should'nt play it seriously.\nAnyway, I felt like the biggest asshole when I killed your rock 'n roll." },
-                    { name = "lucy.txt", type = "file-text",
+                    { name = "about.txt", type = "file-text", size = 21,
+                        content = "This game is shit, and you should'nt play it seriously.\nAnyway, I felt like the biggest asshole when I killed your rock 'n roll.\nThanks for playing tho." },
+                    { name = "lucy.txt", type = "file-text", size = 21,
                         content = "Picture yourself in a boat on a river\nWith tangerine trees and marmalade skies\nSomebody calls you, you answer quite slowly\nA girl with kaleidoscope eyes" },
                 }
             },
@@ -21,18 +23,21 @@ return {
                 name = "Pictures",
                 type = "folder",
                 expanded = false,
+                selected = false,
                 children = {}
             },
             {
                 name = "Music",
                 type = "folder",
                 expanded = false,
+                selected = false,
                 children = {}
             },
             {
                 name = "Videos",
                 type = "folder",
                 expanded = false,
+                selected = false,
                 children = {}
             },
         }
